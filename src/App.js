54cc3c6a8511b806cf7ff7eaa209c3469c263be4
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-08-08 14:04:20 +0900

    초기셋팅 : 불필요한 파일 제거 및 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Link to="/">메인</Link>
+      <Link to="/signup">회원가입</Link>
+      <Link to="/signin">로그인</Link>
+      <Link to="/todo">투두</Link>
+      <Routes>
+        <Route path="/" element={<div>메인임</div>}></Route>
+        <Route path="/signup" element={<div>회원가입</div>}></Route>
+        <Route path="/signin" element={<div>로그인</div>}></Route>
+        <Route path="/todo" element={<div>투두</div>}></Route>
+      </Routes>
     </div>
   );
 }
