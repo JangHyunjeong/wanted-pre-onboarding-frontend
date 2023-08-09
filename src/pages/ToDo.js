8220182b80a26f3e80f184826a62a9f093ad1e68
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-08-09 18:44:10 +0900

    🔨 수정하기 추가 : 체크하기와 수정하기 분리해서 추가해둠.

--- src/pages/ToDo.js
+++ src/pages/ToDo.js
@@ -10,6 +10,8 @@ function ToDo() {
   const [newTodo, setNewTodo] = useState("");
   const token = JSON.parse(localStorage.getItem("access_token"));
   const [todoList, setTodoList] = useState([]);
+  const [isEditable, setIsEditable] = useState("");
+  const [editModeInput, setEditModeInput] = useState("");
 
   // token이 없는 경우, signin 으로 리다이렉트
   useEffect(() => {
@@ -20,6 +22,7 @@ function ToDo() {
     }
   }, []);
 
+  // 리스트 가져오기
   const getTodos = () => {
     axios({
       method: "get",
@@ -36,6 +39,7 @@ function ToDo() {
       });
   };
 
+  // 생성하기
   const createTodo = () => {
     const data = { todo: newTodo };
 
@@ -56,8 +60,49 @@ function ToDo() {
       });
   };
 
+  // 삭제하기
+  const deleteTodo = (id) => {
+    axios({
+      method: "DELETE",
+      url: `https://www.pre-onboarding-selection-task.shop/todos/${id}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        getTodos();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // 수정하기
   const updateTodo = (isCompleted, id) => {
-    const target = todoList.find((item) => {
+    const data = { todo: editModeInput, isCompleted: isCompleted };
+
+    axios({
+      method: "PUT",
+      url: `https://www.pre-onboarding-selection-task.shop/todos/${id}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      data: data,
+    })
+      .then((res) => {
+        getTodos();
+        setIsEditable("");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // 체크하기
+  const updateCompleted = (isCompleted, id) => {
+    const target = [...todoList].find((item) => {
       return item.id === id;
     });
     const data = { todo: target.todo, isCompleted: isCompleted };
@@ -79,21 +124,19 @@ function ToDo() {
       });
   };
 
-  const deleteTodo = (id) => {
-    axios({
-      method: "DELETE",
-      url: `https://www.pre-onboarding-selection-task.shop/todos/${id}`,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((res) => {
-        console.log(res);
-        getTodos();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  // 수정할 input 가져오기
+  const getEditModeInput = (value) => {
+    setEditModeInput(value);
+  };
+
+  // 수정모드로 전환
+  const changeMode = (id) => {
+    const target = [...todoList].find((item) => {
+      return item.id === id;
+    });
+
+    setIsEditable(id);
+    setEditModeInput(target.todo);
   };
 
   return (
@@ -117,101 +160,83 @@ function ToDo() {
         {todoList.map((item) => {
           return (
             <li className={style.todo_item} key={item.id}>
-              <div className={style.todo_normal}>
-                <label>
-                  <div className={style.check_wrap}>
-                    <input
-                      type="checkbox"
-                      className="visually-hidden"
-                      onChange={(e) => {
-                        updateTodo(e.target.checked, item.id);
+              {isEditable !== item.id ? (
+                <div className={style.todo_normal}>
+                  <label>
+                    <div className={style.check_wrap}>
+                      <input
+                        type="checkbox"
+                        className="visually-hidden"
+                        onChange={(e) => {
+                          updateCompleted(e.target.checked, item.id);
+                        }}
+                      />
+                      {item.isCompleted === false ? (
+                        <i
+                          className={`xi-check-circle-o ${style.check_off}`}
+                        ></i>
+                      ) : (
+                        <i className={`xi-check-circle ${style.check_on}`}></i>
+                      )}
+                    </div>
+
+                    <span>{item.todo}</span>
+                  </label>
+                  <div className={style.button_wrap}>
+                    <button
+                      data-testid="modify-button"
+                      className={style.bg_white}
+                      onClick={() => {
+                        changeMode(item.id);
                       }}
-                    />
-                    {item.isCompleted === false ? (
-                      <i className={`xi-check-circle-o ${style.check_off}`}></i>
-                    ) : (
-                      <i className={`xi-check-circle ${style.check_on}`}></i>
-                    )}
+                    >
+                      수정
+                    </button>
+                    <button
+                      data-testid="delete-button"
+                      className={style.bg_black}
+                      onClick={() => {
+                        deleteTodo(item.id);
+                      }}
+                    >
+                      삭제
+                    </button>
                   </div>
-
-                  <span>{item.todo}</span>
-                </label>
-                <div className={style.button_wrap}>
-                  <button
-                    data-testid="modify-button"
-                    className={style.bg_white}
-                  >
-                    수정
-                  </button>
-                  <button
-                    data-testid="delete-button"
-                    className={style.bg_black}
-                    onClick={() => {
-                      deleteTodo(item.id);
+                </div>
+              ) : (
+                <div className={style.todo_edit}>
+                  <input
+                    data-testid="modify-input"
+                    onInput={(e) => {
+                      getEditModeInput(e.target.value);
                     }}
-                  >
-                    삭제
-                  </button>
+                    value={editModeInput}
+                  />
+                  <div className={style.button_wrap}>
+                    <button
+                      data-testid="submit-button"
+                      className={style.bg_blue}
+                      onClick={() => {
+                        updateTodo(item.isCompleted, item.id);
+                      }}
+                    >
+                      제출
+                    </button>
+                    <button
+                      data-testid="cancel-button"
+                      className={style.bg_black}
+                      onClick={() => {
+                        setIsEditable("");
+                      }}
+                    >
+                      취소
+                    </button>
+                  </div>
                 </div>
-              </div>
-
-              <div className={style.todo_edit}>
-                <input data-testid="modify-input" />
-                <div className={style.button_wrap}>
-                  <button data-testid="submit-button" className={style.bg_blue}>
-                    제출
-                  </button>
-                  <button
-                    data-testid="cancel-button"
-                    className={style.bg_black}
-                  >
-                    취소
-                  </button>
-                </div>
-              </div>
+              )}
             </li>
           );
         })}
-
-        {/* <li className={style.todo_item}>
-          <div className={style.todo_normal}>
-            <label>
-              <div className={style.check_wrap}>
-                <input type="checkbox" className="visually-hidden" />
-
-                <i className={`xi-check-circle-o ${style.check_off}`}></i>
-                <i className={`xi-check-circle ${style.check_on}`}></i>
-              </div>
-
-              <span>
-                TODO 1 TODO 1TODO 1TODO 1TODO 1TODO 1TODO 1TODO 1TODO 1TODO
-                1TODO 1TODO 1TODO 1TODO 1TODO 1TODO 1TODO 1TODO 1TODO 1TODO
-                1TODO 1TODO 1TODO 1TODO 1TODO 1TODO 1TODO 1TODO 1TODO 1TODO
-                1TODO 1TODO 1TODO 1TODO 1TODO 1TODO 1TODO 1TODO 1
-              </span>
-            </label>
-            <div className={style.button_wrap}>
-              <button data-testid="modify-button" className={style.bg_white}>
-                수정
-              </button>
-              <button data-testid="delete-button" className={style.bg_black}>
-                삭제
-              </button>
-            </div>
-          </div>
-
-          <div className={style.todo_edit}>
-            <input data-testid="modify-input" />
-            <div className={style.button_wrap}>
-              <button data-testid="submit-button" className={style.bg_blue}>
-                제출
-              </button>
-              <button data-testid="cancel-button" className={style.bg_black}>
-                취소
-              </button>
-            </div>
-          </div>
-        </li> */}
       </ul>
     </div>
   );
