c7908b44edc03de00818df43d7c5347552b871f0
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-08-09 22:37:48 +0900

    💄 Main : 메인페이지 스타일 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,22 @@
 import { Link } from "react-router-dom";
-import "../styles/Main.module.css";
+import style from "../styles/Main.module.css";
 
 function Main() {
   return (
-    <div className="container">
+    <div className={style.main_wrap}>
       <h2 className="visually-hidden">메인</h2>
-      <p>로그인 혹은 회원가입을 해주세요</p>
+      <p>
+        서비스 사용을 위해서는 로그인을 해주세요.
+        <br /> 계정이 없다면, 회원가입을 해주세요.{" "}
+      </p>
 
-      <div>
-        <Link to="/signup">회원가입</Link>
-        <Link to="/signin">로그인</Link>
+      <div className={style.button_wrap}>
+        <Link to="/signup" className={style.bg_white}>
+          회원가입
+        </Link>
+        <Link to="/signin" className={style.bg_black}>
+          로그인
+        </Link>
       </div>
     </div>
   );
