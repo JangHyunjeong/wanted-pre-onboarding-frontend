3523f9bc0a2950e49ed5e53de537855e91e9a115
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-08-09 17:33:06 +0900

    Todo : Create, Get 구현

--- src/pages/ToDo.js
+++ src/pages/ToDo.js
@@ -1,17 +1,130 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+// styles
 import style from "../styles/ToDo.module.css";
 
 function ToDo() {
+  const navigate = useNavigate();
+  const [newTodo, setNewTodo] = useState("");
+  const token = JSON.parse(localStorage.getItem("access_token"));
+  const [todoList, setTodoList] = useState([]);
+
+  // token이 없는 경우, signin 으로 리다이렉트
+  useEffect(() => {
+    if (token === null) {
+      navigate(`/signin`);
+    } else {
+      getTodos();
+    }
+  }, []);
+
+  const getTodos = () => {
+    axios({
+      method: "get",
+      url: "https://www.pre-onboarding-selection-task.shop/todos",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        setTodoList(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const createTodo = () => {
+    let data = { todo: newTodo };
+
+    axios({
+      method: "post",
+      url: "https://www.pre-onboarding-selection-task.shop/todos",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      data: data,
+    })
+      .then((res) => {
+        getTodos();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
       <h2 className={style.title}>투두</h2>
 
       <div className={style.todo_write}>
-        <input data-testid="new-todo-input" />
-        <button data-testid="new-todo-add-button">추가</button>
+        <input
+          data-testid="new-todo-input"
+          onInput={(e) => {
+            setNewTodo(e.target.value);
+          }}
+          value={newTodo}
+        />
+        <button data-testid="new-todo-add-button" onClick={() => createTodo()}>
+          추가
+        </button>
       </div>
 
       <ul className={style.todo_list}>
-        <li className={style.todo_item}>
+        {todoList.map((item) => {
+          return (
+            <li className={style.todo_item} key={item.id}>
+              <div className={style.todo_normal}>
+                <label>
+                  <div className={style.check_wrap}>
+                    <input type="checkbox" className="visually-hidden" />
+                    {item.isCompleted === false ? (
+                      <i className={`xi-check-circle-o ${style.check_off}`}></i>
+                    ) : (
+                      <i className={`xi-check-circle ${style.check_on}`}></i>
+                    )}
+                  </div>
+
+                  <span>{item.todo}</span>
+                </label>
+                <div className={style.button_wrap}>
+                  <button
+                    data-testid="modify-button"
+                    className={style.bg_white}
+                  >
+                    수정
+                  </button>
+                  <button
+                    data-testid="delete-button"
+                    className={style.bg_black}
+                  >
+                    삭제
+                  </button>
+                </div>
+              </div>
+
+              <div className={style.todo_edit}>
+                <input data-testid="modify-input" />
+                <div className={style.button_wrap}>
+                  <button data-testid="submit-button" className={style.bg_blue}>
+                    제출
+                  </button>
+                  <button
+                    data-testid="cancel-button"
+                    className={style.bg_black}
+                  >
+                    취소
+                  </button>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+
+        {/* <li className={style.todo_item}>
           <div className={style.todo_normal}>
             <label>
               <div className={style.check_wrap}>
@@ -49,7 +162,7 @@ function ToDo() {
               </button>
             </div>
           </div>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
